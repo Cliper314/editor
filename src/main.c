8b8c83392d1f5031fda5cd2c754ad27e302e7c92
Clier314 <3148869690@qq.com>
2025-07-01 16:15:05 +0800

    识别特殊控制键

--- src/main.c
+++ src/main.c
@@ -14,29 +14,7 @@ int main(int argc,char *argv[]) {
   enableRawMode();//进入原始模式
   char c;
 
-
-  while (1) {
-  char c = '\0';
-  if (read(STDIN_FILENO, &c, 1) == -1) die("read");
-  if (iscntrl(c)) {
-    printf("%d", c);
-  } else {
-    printf("%d ('%c')", c, c);
-  }
-  fflush(stdout);
-  if(c != '\n') {
-    tem_row[i++] = c; // 将输入字符添加到临时行
-  }
-  else {
-    tem_row[i] = '\0'; // 结束字符串
-    i = 0;
-    //printf("Adding new line: %s\n", tem_row);
-    add_row(rows, &num_rows, tem_row); // 添加行到rows
-    //printf("New line added.\n");
-    //printf("Current rows: %s\n", rows[num_rows-1].content);
-  }
-  if (c == 'q') break;
-  }
+  read_keyboard_input(rows, &num_rows);
 
   for(int j = 0; j < num_rows; j++) {
     print_row(&rows[j]);
